4d1ffda8f5f31db2c03e0b1f912e5ba4ad13ffad
atharva <atharva.anand2025@gmail.com>
2026-04-13 23:29:22 +0400

    Fixed map physics issues with the slanted parts and added animations to walking and jumping and made it so the character turns left and right depending on where you are moving and added moving platforms and made map abit bigger

--- Script/player.gd
+++ Script/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -8,14 +9,30 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	#Gets the input direction: -1, 0, 1
+	var direction := Input.get_axis("A", "D")
+	
+	#Flips the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true 
+	
+	#Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+	#Applies the movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
